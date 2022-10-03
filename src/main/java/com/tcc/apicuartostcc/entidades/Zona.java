7f0a8788415c8325a3d064cb64484c7b0a570ee0
206 <206@academicos.cesde.edu.co>
2022-10-03 12:17:43 -0500

    cuartostcc

---
+++ src/main/java/com/tcc/apicuartostcc/entidades/Zona.java
@@ -0,0 +1,75 @@
+package com.tcc.apicuartostcc.entidades;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="zonas")
+
+public class Zona implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "capacidad")
+    private Double capacidad;
+
+    @Column(name = "disponible")
+    private Double disponible;
+
+    @OneToMany(mappedBy = "zona")
+    @JsonManagedReference
+    private List<Mercancia> mercancias = new ArrayList<Mercancia>();
+
+    public Zona() {
+    }
+
+    public List<Mercancia> getMercancias() {
+        return mercancias;
+    }
+
+    public void setMercancias(List<Mercancia> mercancias) {
+        this.mercancias = mercancias;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(double capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public double getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(double disponible) {
+        this.disponible = disponible;
+    }
+}
